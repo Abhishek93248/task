50cf7affa7ab0b35ff5a35de847a81e0bd29f841
abhishek <abhishek93248@gmail.com>
2024-03-31 09:27:14 +0530

    bug fixed

--- tests/local-test.py
+++ tests/local-test.py
@@ -3,18 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 
-# Navigate to flipkart.com
+
 driver.get("https://www.flipkart.com/")
-# time.sleep(5)
+
 
 search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("Samsung Galaxy S10")
 search_box.submit()
-# time.sleep(5)
+
 
 mobiles_accessories_link = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, "//a[@title='Mobiles & Accessories']"))
@@ -33,25 +33,16 @@ samsung_filter_div = WebDriverWait(driver, 15).until(
 )
 samsung_filter_div.click()
 
-time.sleep(5)
 
 
 
-# checkbox = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div/div[1]/div/section[4]/label/input"))
-#     )
-# checkbox=driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div/div[1]/div/section[4]/label/input")
-# driver.execute_script("arguments[0].scrollIntoView(true);", checkbox)
-# # time.sleep(10)
-# # print(checkbox)
-# checkbox.click()
+time.sleep(3)
 
-# if not checkbox.is_selected():
+checkbox=driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div/div[1]/div/div[1]/div/section[4]/label")
+driver.execute_script("arguments[0].scrollIntoView(true);", checkbox)
+driver.execute_script("arguments[0].click();", checkbox)
 
-#     checkbox.click()
-
-#     print("Flipkart Assured checkbox selected successfully!")
-
+time.sleep(1)
 
 
 WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@data-id, 'MOB')]")))
@@ -78,14 +69,6 @@ while i<len(results):
         results = driver.find_elements(By.XPATH, "//div[contains(@data-id, 'MOB')]")
 
 
-
-   
-
-
-# for result in results:
-#     print_product(result)
-      # Introduce a 1-second delay between printing each product
-# print_product(results[4])
 driver.quit()
 
 
